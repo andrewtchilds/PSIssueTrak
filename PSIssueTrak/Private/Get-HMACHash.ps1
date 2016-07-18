69e3271471aa6084f40415b04b230490e97a9021
andrewtchilds <andrewtchilds@outlook.com>
2016-07-18 18:23:57 -0500

    building module scaffolding

---
+++ PSIssueTrak/Private/Get-HMACHash.ps1
@@ -0,0 +1,26 @@
+﻿Add-Type -TypeDefinition @'
+    using System;
+    using System.Text;
+    using System.Security.Cryptography;
+
+    public class HMACHash
+    {
+        public static String ComputeHMACHash(String hashKey, String messageToBeHashed)
+        {
+            String hashText = null;
+            Byte[] hashKeyBytes = Encoding.UTF8.GetBytes(hashKey);
+
+            using (HMACSHA512 hmacHashGenerator = new HMACSHA512(hashKeyBytes))
+            {
+                Byte[] messageToBeHashedBytes = Encoding.UTF8.GetBytes(messageToBeHashed);
+                Byte[] hashedMessageBytes = hmacHashGenerator.ComputeHash(messageToBeHashedBytes);
+                hashText = Convert.ToBase64String(hashedMessageBytes);
+            }
+
+            return hashText;
+        }    }'@function Get-HMACHash
+{
+    param([string] $hashKey, [string] $messageToBeHashed)
+
+    return [HMACHash]::ComputeHMACHash($hashKey, $messageToBeHashed)
+}
