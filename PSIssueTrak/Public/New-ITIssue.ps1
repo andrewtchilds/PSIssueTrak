00e527a339375fced93ca3e45f6744ec5cf2ff8b
andrewtchilds <andrewtchilds@outlook.com>
2016-07-22 17:21:24 -0500

    removed function that would attempt to remove variables that were set as empty string

--- PSIssueTrak/Public/New-ITIssue.ps1
+++ PSIssueTrak/Public/New-ITIssue.ps1
@@ -15,7 +15,7 @@ function New-ITIssue {
         [string]$EnteredBy,
         
         [Parameter(Mandatory=$false)]
-        [string]$SeverityID=$null,
+        $SeverityID=$null,
         
         [Parameter(Mandatory=$true)]
         [string]$Subject,
@@ -46,10 +46,10 @@ function New-ITIssue {
         [string]$Priority="3 - Medium",
         
         [Parameter(Mandatory=$false)]
-        [string]$AssetNumber=$null,
+        $AssetNumber=$null,
         
         [Parameter(Mandatory=$false)]
-        [string]$LocationID=$null,
+        $LocationID=$null,
         
         [Parameter(Mandatory=$true)]
         [ValidateScript({ if(Get-ITUser -UserID $_){ $true } })]
@@ -60,20 +60,20 @@ function New-ITIssue {
         [string]$AssignedTo,
         
         [Parameter(Mandatory=$false)]
-        [string]$TargetDate=$null,
+        $TargetDate=$null,
         
         [Parameter(Mandatory=$false)]
-        [string]$RequiredByDate=$null,
+        $RequiredByDate=$null,
         
         [Parameter(Mandatory=$false)]
         [ValidateScript({ if(Get-ITUser -UserID $_){ $true } })]
-        [string]$NextActionTo=$null,
+        $NextActionTo=$null,
         
         [Parameter(Mandatory=$false)]
-        [string]$SubStatusID=$null,
+        $SubStatusID=$null,
         
         [Parameter(Mandatory=$false)]
-        [string]$ProjectID=$null,
+        $ProjectID=$null,
         
         [Parameter(Mandatory=$false)]
         [ValidateScript({
@@ -90,35 +90,27 @@ function New-ITIssue {
         [string]$ShouldNeverSendEmailForIssue="false",
         
         [Parameter(Mandatory=$false)]
-        [string]$ClassID=$null,
+        $ClassID=$null,
         
         [Parameter(Mandatory=$false)]
-        [string]$DepartmentID=$null,
+        $DepartmentID=$null,
         
         [Parameter(Mandatory=$false)]
-        [string]$SpecialFunction1=$null,
+        $SpecialFunction1=$null,
         
         [Parameter(Mandatory=$false)]
-        [string]$SpecialFunction2=$null,
+        $SpecialFunction2=$null,
         
         [Parameter(Mandatory=$false)]
-        [string]$SpecialFunction3=$null,
+        $SpecialFunction3=$null,
         
         [Parameter(Mandatory=$false)]
-        [string]$SpecialFunction4=$null,
+        $SpecialFunction4=$null,
         
         [Parameter(Mandatory=$false)]
-        [string]$SpecialFunction5=$null
+        $SpecialFunction5=$null
     )
 
-    #Need to make sure variables are actually null, otherwise ConvertTo-Json spits out an empty string instead of null...
-    #Ugly, but works for now
-    foreach($h in $MyInvocation.MyCommand.Parameters.GetEnumerator()) {
-        if((Get-Variable $h.key -ErrorAction SilentlyContinue).Value -eq ""){
-            Remove-Variable $h.key
-        }
-    }
-    
     $Body = @{
         ShouldSuppressEmailForCreateOperation="$ShouldSuppressEmailForCreateOperation"
         SubmittedDate=$SubmittedDate
